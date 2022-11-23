715e277ad4c74f6d749dce728ebcc7c2c9d606f2
IceStorman <vladislavmartinisin@gmail.com>
2022-11-23 22:47:04 +0200

    Added day-and-night cycle

--- Assets/Scripts/Global/GameManager.cs
+++ Assets/Scripts/Global/GameManager.cs
@@ -1,12 +1,13 @@
 using ExitGames.Client.Photon;
-using UnityEngine;
 using Photon.Pun;
 
 public class GameManager : MonoBehaviourPunCallbacks
 {
+    public GameParameters gameParameters;
     private void Awake()
     {
         PhotonPeer.RegisterType(typeof(Building), (byte)'B', Utils.Serialize, Utils.Deserialize);
         DataHandler.LoadGameData();
+        GetComponent<DayAndNightCycler>().enabled = gameParameters.enableDayAndNightCycle;
     }
 }
